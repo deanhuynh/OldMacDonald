db590923be3c8816e7709c0bc382becaf3ad274f
dehuynh <dvhdvh123@gmail.com>
2016-10-19 13:53:20 -0700

    Finished

--- Cow.java
+++ Cow.java
@@ -1,4 +1,18 @@
-class Cow //implements Animal 
+class Cow implements Animal//implements Animal 
 {     
-     //your code here
+	protected String myType;
+	protected String mySound;
+	public Cow(String type, String sound)
+	{
+		myType = type;
+		mySound = sound;
+	}
+	public Cow()
+	{
+		myType = "unknown";
+		mySound = "unknown";
+	}
+	public String getType(){return myType;}
+	public String getSound(){return mySound;}
 }
+
--- OldMacDonald.pde
+++ OldMacDonald.pde
@@ -1,6 +1,7 @@
 
 void setup()
 {
-	//your variable declarations here
+	Farm bob = new Farm();
+	bob.animalSounds();
 }
 
